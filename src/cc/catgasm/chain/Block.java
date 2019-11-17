ac38ffc6b99c22d1f1bc9a4f10e77f60663c0b94
Simon <simon.wintereder@gmx.at>
2019-11-17 13:42:07 +0100

    Added Block class

---
+++ src/cc/catgasm/chain/Block.java
@@ -0,0 +1,114 @@
+package cc.catgasm.chain;
+
+import cc.catgasm.util.Hash;
+import cc.catgasm.util.Pair;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Date;
+
+public class Block {
+    private byte[] hash;
+    private byte[] prevHash;
+    private byte[] data;
+    private long timestamp;
+    private long nr;
+    private long nonce;
+
+    private static long current_block = 0;
+    private static long difficulty = 2; //Number represents leading zero-bytes in hash
+
+    public Block(Block prv, byte[] data) {
+        this.prevHash = prv.getHash();
+        this.data = data;
+        this.timestamp = new Date().getTime();
+        this.nr = current_block++;
+        Pair<byte[], Long> pair = mine(this);
+        hash = pair.getFirst();
+        nonce = pair.getSecond();
+    }
+
+    public Block(Block prv, String data) {
+        this(prv, data.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public Block( byte[] prevHash, byte[] data) { //Only used for genesis block
+        this.prevHash = prevHash;
+        this.data = data;
+        this.timestamp = new Date().getTime();
+        this.nr = current_block++;
+        Pair<byte[], Long> pair = mine(this);
+        hash = pair.getFirst();
+        nonce = pair.getSecond();
+    }
+
+    //TODO: Probably very inefficient
+    public static Pair<byte[], Long> mine(Block block) {
+        long nonce = -1;
+        int leadingZerosFound = 0;
+        byte[] hash;
+
+        do {
+            ++nonce;
+            leadingZerosFound = 0;
+
+            hash = new Hash(block.getData(), block.getPrevHash(), new byte[]{ //OK lol TODO
+                    (byte) nonce,
+                    (byte) (nonce >> 8),
+                    (byte) (nonce >> 16),
+                    (byte) (nonce >> 24),
+                    (byte) (nonce >> 32),
+                    (byte) (nonce >> 40),
+                    (byte) (nonce >> 48),
+                    (byte) (nonce >> 56)}).getBytes();
+
+            //Check leading zeros
+            for (int i = 0; i < difficulty; i++) {
+                if(hash[i] == 0) {
+                    ++leadingZerosFound;
+                }
+            }
+        } while (leadingZerosFound != difficulty);
+        return new Pair<>(hash, nonce);
+    }
+
+    public byte[] getHash() {
+        return hash;
+    }
+
+    public byte[] getPrevHash() {
+        return prevHash;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public long getNonce() {
+        return nonce;
+    }
+
+    public long getNr() {
+        return nr;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    @Override
+    public String toString() {
+        return "Block{" +
+                "hash=" + Hash.hashToString(hash) +
+                ", prevHash=" + Hash.hashToString(prevHash) +
+                ", data=" + new String(data, StandardCharsets.UTF_8) +
+                ", timestamp=" + timestamp +
+                ", nr=" + nr +
+                ", nonce=" + nonce +
+                '}';
+    }
+
+    public void setData(String nope) {
+        data = nope.getBytes(StandardCharsets.UTF_8);
+    }
+}
